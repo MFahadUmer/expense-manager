1911272dcd6fb54a472d1cc22a1c7e222e05ce8c
Muhamamd Fahad Umer <eng.fahadumer@gmail.com>
2021-10-25 20:43:10 +0500

    Adds Input Fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction('123', 'Internet', 2000, DateTime.now()),
+    Transaction('124', 'Fuel', 1500, DateTime.now())
+  ];
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,18 +22,92 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My Expense Manager'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Card(
-              child: Text('Chart'),
-              elevation: 5,
-            ),
-            Card(
-              child: Text('Expense List'),
-            )
-          ],
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: double.infinity,
+                child: Card(
+                  child: Text('Chart'),
+                  elevation: 5,
+                ),
+              ),
+              Card(
+                elevation: 5,
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Title'),
+                        controller: titleController,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Amount'),
+                        controller: amountController,
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          print(titleController.text);
+                          print(amountController.text);
+                        },
+                        child: Text('Add Transaction'),
+                        textColor: Colors.purple,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                child: Column(
+                  children: transactions.map((e) {
+                    return Card(
+                        child: Row(children: [
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        child: Text("\$${e.amount}",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.purple)),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            e.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.purple,
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(e.date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      )
+                    ]));
+                  }).toList(),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
